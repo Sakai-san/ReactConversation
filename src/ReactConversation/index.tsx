fce66b03f562eb5229f2e84982b8f19af2d58896
Sakai-san <o_edo@hotmail.com>
2024-05-05 21:44:48 +0200

    ref

--- src/ReactConversation/index.tsx
+++ src/ReactConversation/index.tsx
@@ -44,7 +44,7 @@ const ReactConversation: FC<ReactConversationProps> = ({ qas }) => {
             justifyContent: "flex-end",
           }}
         >
-          <Button onClick={next} variant="contained" endIcon={<ChevronRightIcon />}>
+          <Button onClick={next} variant="outlined" endIcon={<ChevronRightIcon />}>
             Next
           </Button>
         </Box>
