eb7404d974d9d6c4f347bdf031992d85887f4029
Sakai-san <o_edo@hotmail.com>
2024-05-05 20:18:31 +0200

    ref

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 //import "./App.css";
 
+import { useState } from "react";
 import ReactConversation from "./ReactConversation";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
@@ -11,7 +12,7 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import Autocomplete from "@mui/material/Autocomplete";
-import { useState } from "react";
+import Box from "@mui/material/Box";
 
 function App() {
   const [formValues, setFormValues] = useState<Record<string, string | boolean>>();
@@ -24,107 +25,109 @@ function App() {
   console.log("formValues", formValues);
   return (
     <Container>
-      <ReactConversation
-        qas={[
-          (ref) => [
-            <Typography>What's your first name ?</Typography>,
-            <TextField
-              inputRef={ref}
-              required
-              name="firstName"
-              id="first-name"
-              label="First name"
-              inputProps={{ autoComplete: "new-password" }}
-              onChange={updateFormValue}
-            />,
-          ],
+      <Box component="form" noValidate autoComplete="off">
+        <ReactConversation
+          qas={[
+            (ref) => [
+              <Typography>What's your first name ?</Typography>,
+              <TextField
+                inputRef={ref}
+                required
+                name="firstName"
+                id="first-name"
+                label="First name"
+                inputProps={{ autoComplete: "new-password" }}
+                onChange={updateFormValue}
+              />,
+            ],
 
-          (ref) => [
-            <Typography>How long have you been doing Frontend developement ?</Typography>,
-            <TextField
-              inputRef={ref}
-              required
-              name="yearsExperience"
-              id="frontend-experience"
-              label="Years of experience"
-              type="number"
-              onChange={updateFormValue}
-            />,
-          ],
+            (ref) => [
+              <Typography>How long have you been doing Frontend developement ?</Typography>,
+              <TextField
+                inputRef={ref}
+                required
+                name="yearsExperience"
+                id="frontend-experience"
+                label="Years of experience"
+                type="number"
+                onChange={updateFormValue}
+              />,
+            ],
 
-          (ref) => [
-            <Typography>Have you ever hold a leadership position ?</Typography>,
-            <FormControl>
-              <RadioGroup ref={ref} row name="leadership-position-radio-buttons-group">
-                <FormControlLabel value="no" control={<Radio />} label="No" />
-                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-              </RadioGroup>
-            </FormControl>,
-          ],
+            (ref) => [
+              <Typography>Have you ever hold a leadership position ?</Typography>,
+              <FormControl>
+                <RadioGroup ref={ref} row name="leadership-position-radio-buttons-group">
+                  <FormControlLabel value="no" control={<Radio />} label="No" />
+                  <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                </RadioGroup>
+              </FormControl>,
+            ],
 
-          (ref) => [
-            <Typography>Do you need a working sponsorship ?</Typography>,
-            <FormControl>
-              <RadioGroup ref={ref} row name="sponsorship-radio-buttons-group">
-                <FormControlLabel value="no" control={<Radio />} label="No" />
-                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-              </RadioGroup>
-            </FormControl>,
-          ],
+            (ref) => [
+              <Typography>Do you need a working sponsorship ?</Typography>,
+              <FormControl>
+                <RadioGroup ref={ref} row name="sponsorship-radio-buttons-group">
+                  <FormControlLabel value="no" control={<Radio />} label="No" />
+                  <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                </RadioGroup>
+              </FormControl>,
+            ],
 
-          (ref) => [
-            <Typography>When could you start earlier ?</Typography>,
-            <TextField
-              inputRef={ref}
-              required
-              name="startingTime"
-              id="starting-time"
-              type="date"
-              onChange={updateFormValue}
-            />,
-          ],
+            (ref) => [
+              <Typography>When could you start earlier ?</Typography>,
+              <TextField
+                inputRef={ref}
+                required
+                name="startingTime"
+                id="starting-time"
+                type="date"
+                onChange={updateFormValue}
+              />,
+            ],
 
-          (ref) => [
-            <Typography>What are your salary expectation ?</Typography>,
-            <TextField
-              inputRef={ref}
-              required
-              name="expectedSalary"
-              id="salary"
-              label="Salary expectation"
-              InputLabelProps={{ shrink: true }}
-              type="number"
-              onChange={updateFormValue}
-            />,
-          ],
+            (ref) => [
+              <Typography>What are your salary expectation ?</Typography>,
+              <TextField
+                inputRef={ref}
+                required
+                name="expectedSalary"
+                id="salary"
+                label="Salary expectation"
+                InputLabelProps={{ shrink: true }}
+                type="number"
+                onChange={updateFormValue}
+              />,
+            ],
 
-          (ref) => [
-            <Typography>What technologies do you have a professional experience with ?</Typography>,
-            <Autocomplete
-              style={{ width: 500 }}
-              multiple
-              id="technos"
-              options={[
-                "react",
-                "Java EE",
-                "Spring Boot",
-                "php",
-                "laravel",
-                "symphony",
-                "Django",
-                "nodejs",
-                "Express",
-                "Angular",
-                "AngularJS",
-                "python",
-                "TypeScript",
-              ]}
-              filterSelectedOptions
-              renderInput={(params) => <TextField {...params} inputRef={ref} label="Techos" />}
-            />,
-          ],
-        ]}
-      />
+            (ref) => [
+              <Typography>What technologies do you have a professional experience with ?</Typography>,
+              <Autocomplete
+                style={{ width: 500 }}
+                multiple
+                id="technos"
+                options={[
+                  "react",
+                  "Java EE",
+                  "Spring Boot",
+                  "php",
+                  "laravel",
+                  "symphony",
+                  "Django",
+                  "nodejs",
+                  "Express",
+                  "Angular",
+                  "AngularJS",
+                  "python",
+                  "TypeScript",
+                ]}
+                filterSelectedOptions
+                renderInput={(params) => <TextField {...params} inputRef={ref} label="Techos" />}
+              />,
+            ],
+          ]}
+        />
+      </Box>
     </Container>
   );
 }
