2c997b1b69e4a521d8a342b574884b1381f95161
Sakai-san <o_edo@hotmail.com>
2024-05-09 23:06:53 +0200

    Controlled component

---
+++ src/ReactConversation/ControlledAutocomplete.tsx
@@ -0,0 +1,90 @@
+import { ReactElement } from "react";
+import { Controller, useFormContext, UseControllerProps, FieldPath, FieldValues } from "react-hook-form";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+
+type ControlledAutocompleteProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = Pick<UseControllerProps<TFieldValues, TName>, "name" | "defaultValue"> & {
+  TextFieldProps: TextFieldProps;
+};
+
+const ControlledAutocomplete = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({
+  name,
+  defaultValue,
+  TextFieldProps,
+}: ControlledAutocompleteProps<TFieldValues, TName>): ReactElement<
+  ControlledAutocompleteProps<TFieldValues, TName>
+> => {
+  const formContext = useFormContext();
+  const {
+    control,
+    formState: { isSubmitting },
+  } = formContext;
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      defaultValue={defaultValue}
+      render={({ field, fieldState }) => (
+        <Autocomplete
+          {...field}
+          onChange={(event, value, reason, details) => field.onChange(value)}
+          style={{ width: 500 }}
+          multiple
+          id="technos"
+          options={[
+            "react",
+            "Java EE",
+            "Spring Boot",
+            "php",
+            "laravel",
+            "symphony",
+            "Django",
+            "nodejs",
+            "Express",
+            "Angular",
+            "AngularJS",
+            "python",
+            "TypeScript",
+          ]}
+          disabled={isSubmitting}
+          filterSelectedOptions
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              inputRef={ref}
+              label="Techos"
+              helperText={fieldState.error?.message ?? " "}
+              error={Boolean(fieldState.error)}
+            />
+          )}
+        />
+      )}
+    />
+  );
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      defaultValue={defaultValue}
+      render={({ field, fieldState }) => (
+        <TextField
+          {...field}
+          helperText={fieldState.error?.message ?? " "}
+          error={Boolean(fieldState.error)}
+          disabled={isSubmitting}
+          {...TextFieldProps}
+        />
+      )}
+    />
+  );
+};
+
+export default ControlledAutocomplete;
