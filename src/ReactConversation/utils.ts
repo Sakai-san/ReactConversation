bbdde7b0f18db1066e2ad9ffb8e12dcf81a8c25e
Sakai-san <o_edo@hotmail.com>
2024-05-12 11:08:45 +0200

    form

--- src/ReactConversation/utils.ts
+++ src/ReactConversation/utils.ts
@@ -1,4 +1,3 @@
-import { MutableRefObject } from "react";
 import { RefCallBack } from "react-hook-form";
 
 export const decorateCallbackRef =
@@ -9,11 +8,3 @@ export const decorateCallbackRef =
     refCallback(element[0]);
     setInputNode(currentPosition, element[0]);
   };
-
-// export const decorateCallbackRef =
-//   (componentRef: MutableRefObject<HTMLElement | undefined>) =>
-//   (refCallback: RefCallBack) =>
-//   (...element: Parameters<RefCallBack>) => {
-//     refCallback(element[0]);
-//     componentRef.current = element[0];
-//   };
