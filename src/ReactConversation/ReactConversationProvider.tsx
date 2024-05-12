22f939828e0c10300a848d8a044bddb1fe940c18
Sakai-san <o_edo@hotmail.com>
2024-05-12 22:52:19 +0200

    form

--- src/ReactConversation/ReactConversationProvider.tsx
+++ src/ReactConversation/ReactConversationProvider.tsx
@@ -56,4 +56,4 @@ function useReactConversation() {
   return context;
 }
 
-export { ReactConversationContext, useReactConversation };
+export { ReactConversationContext, ReactConversationProvider, useReactConversation };
