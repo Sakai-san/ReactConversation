19a90735cca2808d55bedccd63d170eb48b0327f
Sakai-san <o_edo@hotmail.com>
2024-05-12 11:08:26 +0200

    form

--- src/ReactConversation/useFocusOnMount.ts
+++ src/ReactConversation/useFocusOnMount.ts
@@ -1,18 +1,3 @@
-// import { useEffect, useRef } from "react";
-// import { useReactConversation } from "./ReactConversationProvider";
-
-// export const useFocusOnMount = () => {
-//   const componentRef = useRef<HTMLElement>();
-
-//   const { currentPosition, getInputNode } = useReactConversation();
-
-//   useEffect(() => {
-//     //    getInputNode(currentPosition)?.querySelector?.("input")?.focus();
-//     componentRef.current?.querySelector?.("input")?.focus();
-//   }, []);
-//   return componentRef;
-// };
-
 import { useEffect } from "react";
 import { useReactConversation } from "./ReactConversationProvider";
 
